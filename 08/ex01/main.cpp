80bc880f0703a0aa70e01080a2b8c0bbfa07e873
Justus de Groot <justus@macbook.local>
2023-04-29 23:03:42 +0200

    ok

--- 08/ex01/main.cpp
+++ 08/ex01/main.cpp
@@ -5,13 +5,15 @@
 
 int main(void)
 {
-	Span sp = Span(5);
+	Span sp = Span(200);
 
 	sp.addNumber(6);
 	sp.addNumber(3);
 	sp.addNumber(17);
 	sp.addNumber(9);
 	sp.addNumber(11);
+
+	sp.fillSpan(195);
 
 	std::cout << sp.shortestSpan() << std::endl;
 	std::cout << sp.longestSpan() << std::endl;
